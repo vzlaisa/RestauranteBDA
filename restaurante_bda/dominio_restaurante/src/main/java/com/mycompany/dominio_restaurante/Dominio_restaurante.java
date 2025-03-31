182f6b741c88e9c8d7baa380e5bdec440d6ee2c1
vzlaisa <147114185+vzlaisa@users.noreply.github.com>
2025-03-30 22:42:32 -0700

    Creación de modulos
    Inicio de codificación del proyecto
    - Creación de los proyectos para cada capa

---
+++ restaurante_bda/dominio_restaurante/src/main/java/com/mycompany/dominio_restaurante/Dominio_restaurante.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dominio_restaurante;
+
+/**
+ *
+ * @author rocha
+ */
+public class Dominio_restaurante {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ restaurante_bda/persistencia_restaurante/src/main/java/com/mycompany/persistencia_restaurante/Persistencia_restaurante.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.persistencia_restaurante;
+
+/**
+ *
+ * @author rocha
+ */
+public class Persistencia_restaurante {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
